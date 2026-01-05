e7433a1286a153c67e7362642ad7aeb65c624beb
Dixon <Dixon_chu@wiwynn.com>
2026-01-06 00:17:26 +0800

    Update Vite configuration to set base path for repository

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/stock-analysis-app/' : '/', // 如果 GitHub repo 名稱不是 stock-analysis-app，請修改這裡
+  base: '/STOCK_6669/', // 這裡一定要對應你的 Repository 名稱
 })
-
